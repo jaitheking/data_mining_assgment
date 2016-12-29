0a4ea276c1c2886a2c4e727ca9e3079c4cbf554e
jaitheking <jaitheking4858@gmail.com>
2016-12-30 02:22:07 +0800

    Added my part for reference - Data Preprocessing

--- jai_part.R
+++ jai_part.R
@@ -1,21 +1,39 @@
 # Data Preprocessing
 # Initializating the dataset into R Studio
 # Load table as imdb and view the dataset
-imdb<- read.csv(file='imdb-5000-movie-dataset/movie_metadata.csv', header=TRUE)
-View(imdb)
+imdb<- read.csv(file='imdb-5000-movie-dataset/movie_metadata.csv', header=TRUE, stringsAsFactors = FALSE)
+# Uncomment below if you want to view the dataset
+#View(imdb) 
 
 # Check the structure of the variables and summary
 # If structure of a variable unsuitable, 
 # use imdb$<column> <- as.numeric(imdb$<column>) to change the structure
 
 str(imdb)
-summary(imdb)
-hist
 # Make a subset of the original dataset and preprocess the first 10 column of the dataset,
 
-first_ten <- imdb[1:500, 1:10]
-View(first_ten)
+first_ten <- imdb[, 1:10]
+
+# Change the observation of the color Variable to LOWERCASE and convert column structure into factor type.
+
+first_ten$color<- tolower(first_ten$color)
+first_ten$color<-as.factor(first_ten$color)
+first_ten$color<-droplevels(first_ten$color)
+first_ten$director_name<-as.factor(first_ten$director_name)
+
+# We attempt to make an intuitive decision to subset the data frame by filtering out observations will not contributing our data mining process later on. So we will set our own conditions such as:
+# director_facebook_likes >= 500
+# actor_1_facebook_likes >= 500
+# actor_3_facebook_likes >= 500
+# duration >= 30
+# gross >= 1000000
+
+#Subset the first_ten_complete  
+
+first_ten_sub <- subset(first_ten, director_facebook_likes >= 500 & actor_1_facebook_likes >= 500 & actor_3_facebook_likes >=500 & duration >=30 & gross >= 1000000 )
 
 # Eliminate rows with missing values
-first_ten_complete <- na.omit(first_ten)
+first_ten_clean <- na.omit(first_ten_sub)
+
+summary(first_ten_clean)
 
